6ec862d71b6b1ddd76b68a174057037d557d76ac
Thomas Nordentoft <2400757n@student.gla.ac.uk>
2020-03-06 11:19:39 +0000

    added some code helping code from tango project

---
+++ ourplace_project/ourplace/tests.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from rango.models import Category
+from django.urls import reverse
+
+
+class CategoryMethodTests(TestCase):
+    def test_ensure_views_are_positive(self):
+        """
+        Ensures the number of views received for a Category are positive or zero.
+        """
+        category = Category(name='test', views=-1, likes=0)
+        category.save()
+
+        self.assertEqual((category.views >= 0), True)
+
+class IndexViewTests(TestCase):
+    def test_index_view_with_no_categories(self):
+        """
+        If no categories exist, the appropriate message should be displayed.
+        """
+        response = self.client.get(reverse('rango:index'))
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'There are no categories present.')
+        self.assertQuerysetEqual(response.context['categories'], [])
+
