9a6349ebff32712b6924bbe69c20642263e44aa2
Hauwa Kulu Njidda <2431113n@student.gla.ac.ukgit config --global user.name Hauwa Kulu Njidda>
2020-03-10 18:59:28 +0000

    Chapter 10

--- ourplace_project/ourplace/forms.py
+++ ourplace_project/ourplace/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from rango.models import Page, Category
+from ourplace.models import Page, Category
 from django.contrib.auth.models import User 
-from rango.models import UserProfile
+from ourplace.models import UserProfile
 
 
 class CategoryForm(forms.ModelForm):
