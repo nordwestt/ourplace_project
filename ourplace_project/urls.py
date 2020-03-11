ab6ee37442aab55878b150fc7b9fcd18a2e51535
AngusICampbell <2395398c@student.gla.ac.uk>
2020-03-11 12:50:55 +0000

    Did some tidying up

--- ourplace_project/urls.py
+++ ourplace_project/urls.py
@@ -20,5 +20,5 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('rango/', include('ourplace.urls')),
+    path('ourplace/', include('ourplace.urls')),
 ]
