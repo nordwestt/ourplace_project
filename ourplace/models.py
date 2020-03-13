9c8ce5d90ecc3f97d3e5fb52cc47150d2bd9053d
Hauwa Kulu Njidda <2431113n@student.gla.ac.ukgit config --global user.name Hauwa Kulu Njidda>
2020-03-13 19:08:50 +0000

    Update

--- ourplace/models.py
+++ ourplace/models.py
@@ -5,6 +5,35 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Category(models.Model): 
+    NAME_MAX_LENGTH = 128
+
+    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True) 
+    views = models.IntegerField(default=0) 
+    likes = models.IntegerField(default=0) 
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs): 
+        self.slug = slugify(self.name) 
+        super(Category, self).save(*args, **kwargs)
+
+    class Meta: 
+        verbose_name_plural = 'categories'
+
+    def __str__(self): 
+        return self.name
+
+    
+class Page(models.Model):    
+    TITLE_MAX_LENGTH = 128
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=TITLE_MAX_LENGTH)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+        
+    def __str__(self):
+        return self.title
 
 
 class UserProfile(models.Model): 
