1c7c0eb8ffc96eeb43ae3eaa8504996f1eb3d0d5
MHCooke <grapesdigital@gmail.com>
2020-03-30 18:53:08 +0100

    Add public user profile pages

--- ourplace/views.py
+++ ourplace/views.py
@@ -3,34 +3,47 @@ from django.http import HttpResponse
 from django.urls import reverse
 from django.http import Http404
 from django.contrib.auth.decorators import login_required
+from django.db.models import Sum
 
 import static.constants.colours as palettes
 
-context_dict = {}
-
-from ourplace.models import Canvas
+from django.contrib.auth.models import User
+from ourplace.models import Canvas, UserProfile
 from ourplace.forms import CanvasForm
 
 def index(request):
-    response = render(request, 'ourplace/index.html', context=context_dict)
+    response = render(request, 'ourplace/index.html')
     return response
 
 def about(request):
     return render(request, 'ourplace/about.html', context=context_dict)
 
 def faq(request):
-    return render(request, 'ourplace/faq.html', context=context_dict)
+    return render(request, 'ourplace/faq.html')
 
 @login_required
 def account(request):
-    return render(request, 'ourplace/account.html', context=context_dict)
+    return render(request, 'ourplace/account.html')
 
-def user(request):
+def user(request, username):
+    context_dict = {}
+
+    try:
+        target_user = User.objects.get(username=username)
+        context_dict['target_users_places'] = Canvas.objects.filter(owner=target_user).order_by('-views')
+        context_dict['num_target_users_places'] = len(context_dict['target_users_places'])
+        context_dict['target_user_total_views'] = context_dict['target_users_places'].aggregate(Sum('views'))['views__sum']
+        context_dict['target_user'] = target_user
+        # context_dict['target_user_profile'] = UserProfile.objects.get(user=target_user)
+    except User.DoesNotExist:
+        context_dict['target_user'] = None
+
     return render(request, 'ourplace/user.html', context=context_dict)
 
 @login_required
 def create_place(request):
     form = CanvasForm()
+    context_dict = {}
     context_dict['form'] = form
 
     # do we have a http post
