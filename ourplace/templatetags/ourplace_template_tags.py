6660af70f4b3b0d9d23acdd103d0b6db0cf9d337
MHCooke <grapesdigital@gmail.com>
2020-03-31 23:29:01 +0100

    Extract form to template tag

--- ourplace/templatetags/ourplace_template_tags.py
+++ ourplace/templatetags/ourplace_template_tags.py
@@ -3,6 +3,10 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag('ourplace/place_carousel.html')
-def display_carousel(place_list=None, include_author=True):
-    return {'place_list':place_list,
-            'include_author': include_author}
+def display_carousel(place_list, user):
+    return_dict = {'place_list': place_list, 'user':user}
+    return return_dict
+
+@register.inclusion_tag('ourplace/styled_form.html')
+def styled_form(form):
+    return {'form': form}
